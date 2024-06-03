10d723e674adef07b673f1729d0d9fae18c4724e
Prasad T <prasad.yoaspire@gmail.com>
2024-06-03 11:56:38 +0530

    fault tolerence added.

--- springboot/microservices/student-registration-app/registration-service/src/main/java/com/abc/registration/controller/RegistrationController.java
+++ springboot/microservices/student-registration-app/registration-service/src/main/java/com/abc/registration/controller/RegistrationController.java
@@ -1,6 +1,9 @@
 package com.abc.registration.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.abc.registration.entity.RegistrationEntity;
+import com.abc.registration.model.Course;
 import com.abc.registration.model.Registration;
 import com.abc.registration.service.RegistrationService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 
 @RestController
@@ -43,6 +50,25 @@ public class RegistrationController {
 	@GetMapping("/all")
 	public List<RegistrationEntity> getRegistrationDetails() {
 		return registrationService.findAllRegistrations();
+	}
+	
+	
+	@GetMapping("/courses")
+	//@CircuitBreaker(name="courseClient", fallbackMethod = "getAllCoursesFallBack")
+	@Retry(name="courseClient", fallbackMethod = "getAllCoursesFallBack")
+	public List<Course> getAllCourses() {		
+		List<Course> courses = restTemplate.getForObject("http://localhost:7072/course/all", ArrayList.class);
+		return courses;		
+	}
+	
+	public List<Course> getAllCoursesFallBack(Exception e) {
+		return Stream.of(
+				new Course(10,"Course-1",1000,30),
+				new Course(20,"Course-2",1000,30),
+				new Course(30,"Course-3",1000,30),
+				new Course(40,"Course-4",1000,30),
+				new Course(50,"Course-5",1000,30)
+				).collect(Collectors.toList());				
 	}
 
 }
