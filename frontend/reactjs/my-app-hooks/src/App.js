1a95f56bd4095c6014c83baa961ea8d37ded62cf
Prasad T <prasad.yoaspire@gmail.com>
2024-06-18 12:31:43 +0530

    React hooks project added.

---
+++ frontend/reactjs/my-app-hooks/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My React App2</h2>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/reactjs/my-app-hooks/src/components/Counter.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function Counter() {
+
+    const[counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(counter+1);
+    }
+
+    const decrement = () => {
+        setCounter(counter-1);
+    }
+
+    return(
+        <div>
+            <h3>Counter : {counter} </h3>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+        </div>
+    )
+}
+
+export default Counter;
