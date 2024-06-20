51bf9319dda1cb8bba41b39cb71586a36caa03f7
Prasad T <prasad.yoaspire@gmail.com>
2024-06-20 17:23:41 +0530

    Yup library added for form validation

--- frontend/reactjs/my-app-hooks/src/components/Login.js
+++ frontend/reactjs/my-app-hooks/src/components/Login.js
@@ -1,24 +1,38 @@
 import { Formik, ErrorMessage, Form, Field } from 'formik';
+import * as Yup from "yup";
 
 function Login() {
+    
+    const LoginSchema = Yup.object().shape({
+        email: Yup.string()
+            .email("Invalid email address format")
+            .required("Email is required"),
+        pwd: Yup.string()
+            .min(3, "Password must be 3 characters at minimum")
+            .required("Password is required"),
+    });
+
     return (
         <>
             <Formik
                 initialValues={{ email: '', pwd: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                        errors.email = 'Email Required';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    if(!values.pwd) {
-                       errors.pwd = 'Password Required' ;
-                    }
-                    return errors;
-                }}
+
+                validationSchema={LoginSchema}
+
+                // validate={values => {
+                //     const errors = {};
+                //     if (!values.email) {
+                //         errors.email = 'Email Required';
+                //     } else if (
+                //         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                //     ) {
+                //         errors.email = 'Invalid email address';
+                //     }
+                //     if(!values.pwd) {
+                //        errors.pwd = 'Password Required' ;
+                //     }
+                //     return errors;
+                // }}
 
                 onSubmit={(values) => {
                     alert(JSON.stringify(values, null, 2));
